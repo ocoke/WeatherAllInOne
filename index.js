f4bc0b7c5e53a9e547870bec500e9da7f6c45386
CKY <awa@outlook.ie>
2022-11-12 10:05:57 +0800

    :tada: 支持任意格式数据传入

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const CONFIG = yaml.load(fs.readFileSync("./_config.yml"));
 const http = require("http");
 const {getMojiToday, getMoji7days} = require("./src/moji");
 const { getWeatherComCN, getWeatherComCN7days } = require("./src/weather_com_cn");
+const ChinaCityList = JSON.parse(fs.readFileSync("./src/lib/ChinaCityList.json"));
 const METHODS = {
     moji: {
         
@@ -32,30 +33,89 @@ const serve = async (req, res) => {
         returnRes(405, "Error!", 405);
         return false;
     }
+    function getCity(data, from, to) {
+        if (from == "code") {
+            if (to == "code") return data;
+            // 使用城市编码获取
+            let _list = {};
+            for (i of ChinaCityList) {
+                for (o of i.city) {
+                    for (j of o.county) {
+                        if (j.code == data) {
+                            _list = j;
+                        }
+                    }
+                }
+            }
+            if (!to) return _list;
+            if (to == "name") return _list.name || CONFIG.defaultCity;
+            if (to == "name_en") return _list.name_en || getCity(CONFIG.defaultCity, "name", "name_en");
+        } else if (from == "name") {
+            if (to == "name") return data;
+            // 使用城市编码获取
+            let _list = {};
+            for (i of ChinaCityList) {
+                for (o of i.city) {
+                    for (j of o.county) {
+                        if (j.name == data) {
+                            _list = j;
+                        }
+                    }
+                }
+            }
+            if (!to) return _list;
+            if (to == "code") return _list.code || getCity(CONFIG.defaultCity, "name", "code");
+            if (to == "name_en") return _list.name_en || getCity(CONFIG.defaultCity, "name", "name_en");
+        } else if (from == "name_en") {
+            if (to == "name_en") return data;
+            // 使用城市编码获取
+            let _list = {};
+            for (i of ChinaCityList) {
+                for (o of i.city) {
+                    for (j of o.county) {
+                        if (j.name_en == data) {
+                            _list = j;
+                        }
+                    }
+                }
+            }
+            if (!to) return _list;
+            if (to == "code") return _list.code || getCity(CONFIG.defaultCity, "name", "code");
+            if (to == "name") return _list.name || CONFIG.defaultCity;
+        }
+        return "";
+    }
     console.log(`[INFO] Req: ${req.url};`);
     if (req.url.startsWith("/api/today")) {
-        var city = param("/api/today").city || CONFIG.defaultCity;
+        var city = getCity(decodeURIComponent(param("/api/today").city) || CONFIG.defaultCity, param("/api/today").format || "name");
         // 默认 API: [0]
         let data;
         if (CONFIG.enable[0] == "moji") {
-            data = await getMojiToday(city);
+            data = await getMojiToday(city.name);
         } else if (CONFIG.enable[0] == "weather_com_cn") {
-            data = await getWeatherComCN(city);
+            data = await getWeatherComCN((city.code).split("CN")[1]);
         }
         returnRes(200, (data));
         return true;
     }
     if (req.url.startsWith("/api/7d")) {
-        var city = param("/api/7d").city || CONFIG.defaultCity;
+        var city = getCity(param("/api/7d").city, param("/api/7d").format);
         let data;
         if (CONFIG.enable[0] == "moji") {
-            data = await getMoji7days(city);
+            data = await getMoji7days(city.name);
         } else if (CONFIG.enable[0] == "weather_com_cn") {
-            data = await getWeatherComCN7days(city);
+            data = await getWeatherComCN7days((city.code).split("CN")[1]);
         }
         returnRes(200, (data));
         return true;
     }
+    if (req.url.startsWith("/api/geo")) {
+        // 城市地理数据 包含城市编号 英文 中文
+        var city = getCity(param("/api/geo").city, param("/api/geo").format);
+        returnRes(200, (city));
+        return true;
+    }
+    returnRes(200, "WeatherAllInOne(WAIO.js) is running.");
 };
 
 http.createServer(async (req, res) => {
--- src/moji.js
+++ src/moji.js
@@ -6,62 +6,10 @@ const getMojiToday = async (city) => {
     var wdata = await fetch(`https://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=${city}&needMoreData=true&pageNo=1&pageSize=1`).then(res => res.json());
     wdata = wdata.data.list[0];
     var data = {};
-    var wind = wdata["wind"].split("风");
-    wind[0] += "风";
-    data = {
-        weather: wdata.weather,
-        temp: {
-            now: wdata.temp ? String(wdata.temp) : null,
-            day: String(wdata.high),
-            night: String(wdata.low),
-        },
-        wind,
-        aq: [wdata.airData, wdata.airQuality],
-        humidity: wdata.humidity,
-        sun: {
-            sunrise: wdata.moreData.sunrise.split(wdata.date+" ")[1].split(":00")[0],
-            sunset: wdata.moreData.sunset.split(wdata.date+" ")[1].split(":00")[0],
-        }
-    };
-    return data;
-}
-
-// const getMojiToday = (city) => {
-//     var dt = fetch(`https://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=${city}&needMoreData=true&pageNo=1&pageSize=1`)
-//         .then(res => res.json()).then(wdata => wdata.data.list[0])
-//         .then(wdata => {
-//             var data = {};
-//             var wind = wdata["wind"].split("风");
-//             wind[0] += "风";
-//             data = {
-//                 weather: wdata.weather,
-//                 temp: {
-//                     now: wdata.temp ? String(wdata.temp) : null,
-//                     day: String(wdata.high),
-//                     night: String(wdata.low),
-//                 },
-//                 wind,
-//                 aq: [wdata.airData, wdata.airQuality],
-//                 humidity: wdata.humidity,
-//                 sun: {
-//                     sunrise: wdata.moreData.sunrise.split(wdata.date+" ")[1].split(":00")[0],
-//                     sunset: wdata.moreData.sunset.split(wdata.date+" ")[1].split(":00")[0],
-//                 }
-//             };
-//             return data;
-//         });
-//     return dt;
-// }
-const getMoji7days = async (city) => {
-    var wdata = await fetch(`https://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=${city}&needMoreData=true&pageNo=1&pageSize=7`).then(res => res.json());
-    var w_data = wdata.data.list;
-    var data = [];
-    for(i in w_data) {
-        wdata = w_data[i];
+    try {
         var wind = wdata["wind"].split("风");
         wind[0] += "风";
-        day = {
-            date: wdata.date,
+        data = {
             weather: wdata.weather,
             temp: {
                 now: wdata.temp ? String(wdata.temp) : null,
@@ -76,16 +24,41 @@ const getMoji7days = async (city) => {
                 sunset: wdata.moreData.sunset.split(wdata.date+" ")[1].split(":00")[0],
             }
         };
-        data.push(day)
+    } catch (e) {}
+    return data;
+}
+const getMoji7days = async (city) => {
+    var wdata = await fetch(`https://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=${city}&needMoreData=true&pageNo=1&pageSize=7`).then(res => res.json());
+    var w_data = wdata.data.list;
+    var data = [];
+    try {
+        for(i in w_data) {
+            wdata = w_data[i];
+            var wind = wdata["wind"].split("风");
+            wind[0] += "风";
+            day = {
+                date: wdata.date,
+                weather: wdata.weather,
+                temp: {
+                    now: wdata.temp ? String(wdata.temp) : null,
+                    day: String(wdata.high),
+                    night: String(wdata.low),
+                },
+                wind,
+                aq: [wdata.airData, wdata.airQuality],
+                humidity: wdata.humidity,
+                sun: {
+                    sunrise: wdata.moreData.sunrise.split(wdata.date+" ")[1].split(":00")[0],
+                    sunset: wdata.moreData.sunset.split(wdata.date+" ")[1].split(":00")[0],
+                }
+            };
+            data.push(day)
+        }
+    } catch (e) {
+        
     }
     return data;
 }
-// (async() => {
-//     console.log("__Start;");
-//     var d1=new Date().getTime();
-//     console.log(await getMoji7days("广州"));
-//     console.log("__End; Total: "+String(new Date().getTime()-d1));
-// })();
 
 module.exports.getMojiToday = async (city) => {
     return await getMojiToday(city);
